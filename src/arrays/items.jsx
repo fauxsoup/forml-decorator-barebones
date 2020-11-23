d050e92418ae13d86a8e82c8b84af7ca181f91cd
Zachary Hueras <zhueras@amdtelemedicine.com>
2020-11-23 17:59:47 -0500

    Fix eslint

--- src/arrays/items.jsx
+++ src/arrays/items.jsx
@@ -1,25 +1,22 @@
-import React, { forwardRef } from 'react';
-import { useLocalizer } from '@forml/hooks';
+import React, { forwardRef } from "react";
+import { useLocalizer } from "@forml/hooks";
 
 /**
  * @component
  */
 function Items(props, ref) {
-    const { getLocalizedString } = useLocalizer();
-    const { disabled } = props;
-    return (
-        <div className="array" ref={ref}>
-            <button
-                key="add"
-                className="add"
-                disabled={disabled}
-                onClick={props.add}
-            >
-                {getLocalizedString('Add Item')}
-            </button>
-            <ul key="items">{props.children}</ul>
-        </div>
-    );
+  const { disabled } = props;
+
+  const { getLocalizedString } = useLocalizer();
+
+  return (
+    <div className="array" ref={ref}>
+      <button key="add" className="add" disabled={disabled} onClick={props.add}>
+        {getLocalizedString("Add")}
+      </button>
+      <ul key="items">{props.children}</ul>
+    </div>
+  );
 }
 
 export default forwardRef(Items);
